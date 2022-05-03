2028711b8fffeae7fde346a339aac7b3bf752b6f
Sofranz <101176196+Sofranz@users.noreply.github.com>
2022-05-03 07:33:03 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,5 @@
+
+import plotly.express as px
 from tkinter import *
 root=Tk() #creating the main window and storing the window object in 'win'
 root.title("Login")
@@ -133,26 +135,35 @@ def graphing():
 
 
 
+    x = Entry(root, width=50)
+    x.place(bordermode=OUTSIDE, height=20, width=300,x=860,y=320)
 
-    def inputx():
-        inputx = Label(root, text=x.get())
-        input.place()
 
-    x= Entry(root, width=50)
-    x.place(bordermode=OUTSIDE,  width=300,x=860,y=250)
 
-    button= Button(root, text="input X values", padx=960 , pady=200,command=inputx , bg='grey')
-    button.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=300)
+    y = Entry(root, width=50)
+    y.place(bordermode=OUTSIDE, height=20, width=300,x=860,y=520)
 
     def inputy():
-        inputx = Label(root, text=y.get())
-        input.place()
+        inputyy = Label(root, text=x.get())
+        inputyy.place()
+        yval=y.get()
 
-    y= Entry(root, width=50)
-    y.place(bordermode=OUTSIDE,  width=300,x=860,y=450)
+        inputxx = Label(root, text=x.get())
+        inputxx.place()
+        xval=x.get()
 
-    button= Button(root, text="input y values", padx=960 , pady=200,command=inputx , bg='grey')
-    button.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=500 , )
+        myArray = xval.split(",")
+
+
+        myArrayy = yval.split(",")
+
+        fig = px.scatter(x=myArray, y=myArrayy)
+        fig.show()
+
+    Button1 = Button( root,command=inputy)
+    Button1.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=620)
+
+
 
     button5= Button(root, text="quit", padx=960 , pady=600, command=close, bg='grey')
     button5.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=750)
@@ -175,7 +186,7 @@ def openn(): # open gui.py
     button.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=210)
 
     button2= Button(root, text="Settings", padx=960 , pady=400,bg='grey')
-    button2.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=420)
+    button2.place()
 
     button3= Button(root, text="quit", padx=960 , pady=600,command=close,bg='grey')
     button3.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=660)
@@ -190,43 +201,42 @@ def login():
         root.title("Graphing")
         def font_style():
            label.config(font=('Helvetica bold', 26))
-    
+
         label = Label(root, text="Login", font=('Times', 24),bg='grey' )
         label.place(bordermode=OUTSIDE,x=910,y=100)
-    
+
         def inputx():
             inputx = Label(root, text=x.get())
             input.place()
-    
+
         x= Entry(root, width=50)
         x.place(bordermode=OUTSIDE,  width=300,x=860,y=250)
-    
+
         button= Button(root, text="Username", padx=960 , pady=200,command=inputx , bg='grey')
         button.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=300)
-    
+
         def inputy():
             inputx = Label(root, text=y.get())
             input.place()
-    
+
         y= Entry(root, width=50)
         y.place(bordermode=OUTSIDE,  width=300,x=860,y=450)
-    
+
         button= Button(root, text="Password", padx=960 , pady=200,command=inputx , bg='grey')
         button.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=500 , )
-    
+
         button5= Button(root, text="quit", padx=960 , pady=600, command=close, bg='grey')
         button5.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=750)
-    
+
         root.configure(bg='grey')
         root.geometry('1920x1080') #setting the size of the window
         root.mainloop() #running the loop that works as a trigge
-    
+
 
 label = Label(root, text="Menu", font=('Times', 24),bg='grey')
 label.place(bordermode=OUTSIDE,x=960,y=100)
 button= Button(root, text="Login", padx=960 , pady=200, command=openn,bg='grey')
 button.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=210,)
-
 
 button2= Button(root, text="quit", padx=960 , pady=400, command=close,bg='grey')#close program after pressing
 button2.place(bordermode=OUTSIDE, height=100, width=300,x=860,y=420)
